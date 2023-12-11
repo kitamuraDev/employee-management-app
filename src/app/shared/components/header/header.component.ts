1485b7039126c88f174cb07d326a23e0de4b4a3b
kitamuraDev <masato1999824@icloud.com>
2023-12-11 19:49:09 +0900

    :bug: ヘッダーリンクが機能しなかったため修正

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchEmployeeComponent } from '../search-employee/search-employee.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchEmployeeComponent],
+  imports: [SearchEmployeeComponent, RouterLink],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {}
